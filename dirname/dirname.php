34322f54925d402afeecf118e8e8e9be11b44d43
beyond88 <muhin.cse.diu@gmail.com>
2021-11-13 11:29:20 +0600

    dirname

---
+++ dirname/dirname.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Dirname levels
+ */
+
+echo dirname('/usr/local/bin').PHP_EOL;
+echo dirname('/usr/local/bin',1).PHP_EOL;
+echo dirname('/usr/local/bin',2).PHP_EOL;
+echo dirname('/usr/local/bin',3).PHP_EOL;
